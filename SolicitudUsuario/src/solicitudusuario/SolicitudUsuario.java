04ed0be9c44f076ea969743f0ed2e04f272c1b6b
3825513540101@ingenieria.usac.edu.gt <3825513540101@ingenieria.edu.gt>
2022-09-03 01:45:03 -0600

    Ejercicio6

---
+++ SolicitudUsuario/src/solicitudusuario/SolicitudUsuario.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package solicitudusuario;
+
+/**
+ *
+ * @author El Samin
+ */
+import java.util.Scanner;
+
+public class SolicitudUsuario {
+
+    /**
+     * 
+     * 
+     */
+    public static void main(String[] args) {
+        
+        
+        Scanner sc = new Scanner(System.in);
+        String UserMain = "s03";
+        String ContraMain = "1";
+        
+        System.out.println("Ingresa usuario");
+        String user1 = sc.next();
+        System.out.println("Ingresa usuario");
+        String contra1 = sc.next();
+        
+        if (UserMain.equals(user1) && ContraMain.equals(contra1)) {
+            System.out.println("Inicio de sesion correcto");
+        }else{
+            System.out.println("Nombre de usuario incorrecto");
+        }
+        
+        
+    }
+    
+}
